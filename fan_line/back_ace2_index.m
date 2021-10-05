2b05c101ee1033d181e9a81b541ed43bea92724f
wangwei-cmd <56121617+wangwei-cmd@users.noreply.github.com>
2021-10-05 18:06:07 +0800

    Update back_ace2_index.m

--- fan_line/back_ace2_index.m
+++ fan_line/back_ace2_index.m
@@ -1,21 +1,35 @@
 function rf=back_ace2_index(pf,index,u_cor,x_cor,y_cor,R,DSD,beta)
-% u_cor=[-rDu:rDu]*delt_u;
-dist=DSD./sqrt(u_cor.^2+DSD^2);
-dist1=(u_cor.^2+DSD^2)./DSD;
 delt_u=u_cor(2)-u_cor(1);
+
+% dist=DSD./sqrt(u_cor.^2+DSD^2);
+% dist1=(u_cor.^2+DSD^2)./DSD;
+
+dist=DSD./sqrt((u_cor+0.5*delt_u).^2+DSD^2);
+dist1=((u_cor+0.5*delt_u).^2+DSD^2)./DSD;
+
 pf=pf.*dist;
 d_beta=zeros(size(pf));
 d_u=zeros(size(pf));
 d_beta(2:end-1,:,:)=(pf(3:end,:,:)-pf(1:end-2,:,:))/2/(beta(2)-beta(1));
-d_u(:,2:end-1)=(pf(:,3:end)-pf(:,1:end-2))/(2*delt_u).*dist1(2:end-1);
+% d_u(:,2:end-1)=(pf(:,3:end)-pf(:,1:end-2))/(2*delt_u).*dist1(2:end-1);
+% d_beta(1:end-1,:,:)=(pf(2:end,:,:)-pf(1:end-1,:,:))/(beta(2)-beta(1));
+d_u(:,1:end-1)=(pf(:,2:end)-pf(:,1:end-1))/(delt_u).*dist1(2:end);
 pf_grad=d_beta+d_u;
 
-NN=300;
+% NN=300;
+% extend_L=[u_cor(1)-NN*delt_u:delt_u:u_cor(1)-delt_u];
+% extend_R=[u_cor(end)+delt_u:delt_u:u_cor(end)+NN*delt_u];
+% u_cor_extend=[extend_L,u_cor,extend_R];%%%%Hilbert filter extend
+% pf_grad=padarray(pf_grad,[0 NN],0,'both');
+% g4=Htransform(pf_grad,u_cor_extend);
+
+NN=10;
+u_cor=u_cor+0.5;
 extend_L=[u_cor(1)-NN*delt_u:delt_u:u_cor(1)-delt_u];
 extend_R=[u_cor(end)+delt_u:delt_u:u_cor(end)+NN*delt_u];
 u_cor_extend=[extend_L,u_cor,extend_R];%%%%Hilbert filter extend
 pf_grad=padarray(pf_grad,[0 NN],0,'both');
-g4=Htransform(pf_grad,u_cor_extend);
+g4=Htransform_matrix(pf_grad,u_cor_extend);
 
 [u_star,v_star]=compute_u(beta,x_cor,y_cor,R,DSD);
 g5=zeros(size(u_star));
